5a68ac9174de5a4047224b476ee39bc20f7579c2
Sai Kiran Kusumanchi <sai.kiran567@gmail.com>
2023-04-15 19:49:30 -0700

    Added the detect and translate language functionality

--- 06-translate-text/Python/text-translation/text-translation.py
+++ 06-translate-text/Python/text-translation/text-translation.py
@@ -39,6 +39,31 @@ def GetLanguage(text):
     language = 'en'
 
     # Use the Translator detect function
+    # Use the Translator detect dcoumentation: https://github.com/MicrosoftTranslator/Text-Translation-Code-Samples/blob/main/Python-Code-Samples/Detect.py
+    path = '/detect'
+    url = translator_endpoint + path
+
+    # Build the request
+    params = {
+        'api-version': '3.0'
+    }
+
+    headers = {
+    'Ocp-Apim-Subscription-Key': cog_key,
+    'Ocp-Apim-Subscription-Region': cog_region,
+    'Content-type': 'application/json'
+    }
+
+    body = [{
+        'text': text
+    }]
+
+    # Send the request and get response
+    request = requests.post(url, params=params, headers=headers, json=body)
+    response = request.json()
+
+    # Parse JSON array and get language
+    language = response[0]["language"]
 
 
     # Return the language
@@ -48,6 +73,32 @@ def Translate(text, source_language):
     translation = ''
 
     # Use the Translator translate function
+    path = '/translate'
+    url = translator_endpoint + path
+
+    # Build the request
+    params = {
+        'api-version': '3.0',
+        'from': source_language,
+        'to': ['en']
+    }
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': cog_key,
+        'Ocp-Apim-Subscription-Region': cog_region,
+        'Content-type': 'application/json'
+    }
+
+    body = [{
+        'text': text
+    }]
+
+    # Send the request and get response
+    request = requests.post(url, params=params, headers=headers, json=body)
+    response = request.json()
+
+    # Parse JSON array and get translation
+    translation = response[0]["translations"][0]["text"]
 
 
     # Return the translation
